8d57da6de24f785194c30f3d7195525d74163f60
ArnoldSimone <sarnold2404@gmail.com>
2024-08-31 14:03:26 +0200

    adddeliveryCosts

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+let emptyBasket = true;
+let subCostsDishes = 0;
+let subCostsTotal = 0;
+let delivery = 0;
+
+function init() {
+  emptyBasket = true;
+  renderDishes();
+  renderEmptyBasket() 
+}
+
 function renderDishes() {
   let dishesRef = document.getElementById("dishes");
-
   dishesRef.innerHTML = "";
 
   for (let indexMenu = 0; indexMenu < menu.length; indexMenu++) {
-    dishesRef.innerHTML += `
-    <div class="ctnMealsCategory">
-          <h1 id="category">${menu[indexMenu].category}</h1>
-          <div id="ctnSingleMeal${indexMenu}" class="ctnSingleMeal"></div>`;
+    dishesRef.innerHTML += getCategoryTemplate(indexMenu);
 
     let ctnSingleMealRef = document.getElementById("ctnSingleMeal" + indexMenu);
     ctnSingleMealRef.innerHTML = "";
@@ -17,40 +24,7 @@ function renderDishes() {
       indexDishes < menu[indexMenu].dishes.length;
       indexDishes++
     ) {
-      ctnSingleMealRef.innerHTML += `
-    
-        <div class="singleMeal">
-              <div class="mealDetails">
-                <p class="meal">${menu[indexMenu].dishes[indexDishes].name}</p>
-                <p class="description">${
-                  menu[indexMenu].dishes[indexDishes].description
-                }</p>
-                <p class="price">${menu[indexMenu].dishes[
-                  indexDishes
-                ].price.toFixed(2).replace(".",",")} €</p>
-              </div>
-              <div class="mealImageAdd">
-                <img
-                  id="imageSingleMenu"
-                  src=${menu[indexMenu].dishes[indexDishes].image}
-                  alt="imageSalat"
-                />
-                
-                <img
-                onclick="addMealToBasket(${indexMenu}, ${indexDishes})"
-                id="imageAmount${indexMenu}${indexDishes}"
-                class="imageAmount"
-                src="./assets/icons/plus-30.png"
-                alt="iconPlus"
-                />
-                <div 
-                    onclick="addMealToBasket(${indexMenu}, ${indexDishes})" 
-                    id="imageAmountNumber${indexMenu}${indexDishes}"
-                    class="numberAmount dnone">
-                </div>
-              </div>
-            </div>
-          </div>`;
+      ctnSingleMealRef.innerHTML += getDishesTemplate(indexMenu, indexDishes);
     }
   }
 }
@@ -58,67 +32,47 @@ function renderDishes() {
 function addMealToBasket(indexMenu, indexDishes) {
   menu[indexMenu].dishes[indexDishes].amount++;
   renderBasket();
+  renderSummary(subCostsTotal);
+  renderEmptyBasket();
 }
 
-
-
 function renderBasket() {
+  emptyBasket = true;
+  subCostsTotal = 0;
+
   let ctnBasketRef = document.getElementById("ctnBasket");
   ctnBasketRef.innerHTML = "";
 
+
   for (let indexMenu = 0; indexMenu < menu.length; indexMenu++) {
     for (
-      let indexDishes = 0;
-      indexDishes < menu[indexMenu].dishes.length;
-      indexDishes++
-    ) {
-      let imageAmountRef = document.getElementById(
-        "imageAmount" + indexMenu + indexDishes
-      );
-      let imageAmountNumberRef = document.getElementById(
-        "imageAmountNumber" + indexMenu + indexDishes
-      );
+      let indexDishes = 0; indexDishes < menu[indexMenu].dishes.length; indexDishes++) {
+      let imageAmountRef = document.getElementById("imageAmount" + indexMenu + indexDishes);
+      let imageAmountNumberRef = document.getElementById("imageAmountNumber" + indexMenu + indexDishes);
+      let ctnSwitchRef = document.getElementById('ctnSwitch');
 
       if (menu[indexMenu].dishes[indexDishes].amount >= 1) {
+        emptyBasket = false;
         imageAmountRef.classList.add("dnone");
         imageAmountNumberRef.classList.remove("dnone");
-        imageAmountNumberRef.innerHTML =
-          menu[indexMenu].dishes[indexDishes].amount;
+        ctnSwitchRef.classList.remove("dnone");
 
-        ctnBasketRef.innerHTML += `
-            <div id="dishesBasket${indexMenu}${indexDishes}" class="dishesBasket">
-              <p class="titleDishesBasket">${menu[indexMenu].dishes[indexDishes].name}</p>
-              <div class="ctnTotalAmountBasket">
-                <div class="totalAmountBasket">
-                  <img onclick="decreaseAmount(${indexMenu}, ${indexDishes})"
-                    class="plusMinusBasket"
-                    src="./assets/icons/minus-30.png"
-                    alt="iconMinus"
-                  />
-                  <p id="singleAmountBasket${indexMenu}${indexDishes}">${menu[indexMenu].dishes[indexDishes].amount}</p>
-                  <img onclick="increaseAmount(${indexMenu}, ${indexDishes})"
-                    class="plusMinusBasket"
-                    src="./assets/icons/plus-30.png"
-                    
-                  />
-                </div>
-                <div class="summaryDish">
-                  <p id="priceAmount">${(menu[indexMenu].dishes[indexDishes].price*menu[indexMenu].dishes[indexDishes].amount).toFixed(2).replace(".",",")} €</p>
-                  <img onclick="deleteDishesOnBasket(${indexMenu}, ${indexDishes})"
-                    class="plusMinusBasket"
-                    src="./assets/icons/trash-48.png"
-                    alt="iconTrash"
-                  />
-                </div>
-              </div>
-            </div>
-            `;
+        imageAmountNumberRef.innerHTML = menu[indexMenu].dishes[indexDishes].amount;
+
+        ctnBasketRef.innerHTML += getBasketTemplate(indexMenu, indexDishes);
+
+        subCostsDishes = menu[indexMenu].dishes[indexDishes].price*menu[indexMenu].dishes[indexDishes].amount;
+        subCostsTotal = (subCostsTotal + subCostsDishes); 
+        renderSummary(subCostsTotal);
+        
       } else {
         imageAmountRef.classList.remove("dnone");
         imageAmountNumberRef.classList.add("dnone");
+
       }
     }
   }
+  renderEmptyBasket();
 }
 
 function decreaseAmount(indexMenu, indexDishes) {
@@ -135,6 +89,46 @@ function deleteDishesOnBasket(indexMenu, indexDishes) {
   menu[indexMenu].dishes[indexDishes].amount = 0;
   renderBasket();
 }
-// console.table(menu[0].dishes)
+
+function renderSummary(subCostsTotal) {
+  let ctnSummaryRef = document.getElementById("ctnSummary");
+  let ctnEmptyBasketRef = document.getElementById("ctnEmptyBasket");
+
+  if (emptyBasket === false) {
+    ctnEmptyBasketRef.innerHTML = "";
+    ctnSummaryRef.innerHTML = getRenderSummaryTemplate();
+  }
+}
+
+function renderEmptyBasket() {
+  let ctnSummaryRef = document.getElementById("ctnSummary");
+  let ctnEmptyBasketRef = document.getElementById("ctnEmptyBasket");
+  let ctnSwitchRef = document.getElementById('ctnSwitch');
+  if (emptyBasket === true) {
+    ctnSwitchRef.classList.add("dnone");
+    ctnSummaryRef.innerHTML = "";
+    ctnEmptyBasketRef.innerHTML = 
+    `           
+    <img src="./assets/icons/basket.png" alt="iconBasket" />
+    <p>Wähle leckere Gerichte aus der Karte und bestelle Dein Menu.</p>`;
+  }
+}
+
+function activate(activeId, inactiveId) {
+  document.getElementById(activeId).classList.add('active');
+  document.getElementById(inactiveId).classList.remove('active');
+  addDeliveryCosts();
+} 
+ 
+function addDeliveryCosts(){
+  if (document.getElementById('delivery').classList.contains('active')) {
+    delivery = 5.90;
+    document.getElementById('deliveryCost').innerHTML = "5,90 €";
+  } else  {
+    document.getElementById('deliveryCost').innerHTML = "0,00 €";
+  }
+}
 
 
+// document.getElementById('delivery').classList.add('active');
+// document.getElementById('pickup').classList.remove('active');
---
+++ scripts/template.js
@@ -0,0 +1,95 @@
+function getCategoryTemplate(indexMenu){
+return `
+    <div class="ctnMealsCategory">
+          <h1 id="category">${menu[indexMenu].category}</h1>
+          <div id="ctnSingleMeal${indexMenu}" class="ctnSingleMeal"></div>`;
+}
+
+function getDishesTemplate(indexMenu, indexDishes){
+    return `
+        <div class="singleMeal">
+              <div class="mealDetails">
+                <p class="meal">${menu[indexMenu].dishes[indexDishes].name}</p>
+                <p class="description">${
+                  menu[indexMenu].dishes[indexDishes].description
+                }</p>
+                <p class="price">${menu[indexMenu].dishes[
+                  indexDishes
+                ].price.toFixed(2).replace(".",",")} €</p>
+              </div>
+              <div class="mealImageAdd">
+                <img
+                  id="imageSingleMenu"
+                  src=${menu[indexMenu].dishes[indexDishes].image}
+                  alt="imageSalat"
+                />
+                
+                <img
+                onclick="addMealToBasket(${indexMenu}, ${indexDishes})"
+                id="imageAmount${indexMenu}${indexDishes}"
+                class="imageAmount"
+                src="./assets/icons/plus-30.png"
+                alt="iconPlus"
+                />
+                <div 
+                    onclick="addMealToBasket(${indexMenu}, ${indexDishes})" 
+                    id="imageAmountNumber${indexMenu}${indexDishes}"
+                    class="numberAmount dnone">
+                </div>
+              </div>
+            </div>
+          </div>`;
+}
+function getBasketTemplate(indexMenu, indexDishes){
+    return `
+            <div id="dishesBasket${indexMenu}${indexDishes}" class="dishesBasket">
+              <p class="titleDishesBasket">${menu[indexMenu].dishes[indexDishes].name}</p>
+              <div class="ctnTotalAmountBasket">
+                <div class="totalAmountBasket">
+                  <img onclick="decreaseAmount(${indexMenu}, ${indexDishes})"
+                    class="plusMinusBasket"
+                    src="./assets/icons/minus-30.png"
+                    alt="iconMinus"
+                  />
+                  <p id="singleAmountBasket${indexMenu}${indexDishes}">${menu[indexMenu].dishes[indexDishes].amount}</p>
+                  <img onclick="increaseAmount(${indexMenu}, ${indexDishes})"
+                    class="plusMinusBasket"
+                    src="./assets/icons/plus-30.png"
+                    
+                  />
+                </div>
+                <div class="summaryDish">
+                  <p id="priceAmount">${(menu[indexMenu].dishes[indexDishes].price*menu[indexMenu].dishes[indexDishes].amount).toFixed(2).replace(".",",")} €</p>
+                  <img onclick="deleteDishesOnBasket(${indexMenu}, ${indexDishes})"
+                    class="plusMinusBasket"
+                    src="./assets/icons/trash-48.png"
+                    alt="iconTrash"
+                  />
+                </div>
+              </div>
+            </div>
+            `;
+}
+
+function getRenderSummaryTemplate(){
+return `
+    <div class="ctnTotal">
+      <div id="subtotal">
+        <p>Zwischensumme</p>
+        <p>${(subCostsTotal).toFixed(2).replace(".",",")} €</p>
+      </div>
+
+      <div id="deliveryCosts">
+        <p>Lieferkosten</p>
+        <p id="deliveryCost">0,00 €</p>
+      </div>
+
+      <div id="totalCosts">
+        <p>Gesamtkosten</p>
+        <p>19,99 €</p>
+      </div>
+    </div>
+    <div class="ctnButtonPay">
+      <button class="pay">Zur Kasse</button>
+    </div>`;
+  }
